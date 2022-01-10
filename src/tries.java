7156203869939160b3e0e022fd4519e02bf2d91e
Srinivasmanju <56474308+Srinivasmanju@users.noreply.github.com>
2022-01-10 13:17:43 +0530

    Update tries.java

--- src/tries.java
+++ src/tries.java
@@ -6,6 +6,8 @@ public class tries {
     System.out.println("hello1");
 		  System.out.println("hello2");
 		  System.out.println("hello3");
+		  System.out.println("hello4");
+		  System.out.println("hello5");
 	}
 
 }
