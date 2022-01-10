dd76663028efea765dda70e456267f204134b0bf
Srinivasmanju <56474308+Srinivasmanju@users.noreply.github.com>
2022-01-10 13:13:58 +0530

    changes

--- src/tries.java
+++ src/tries.java
@@ -4,6 +4,8 @@ public class tries {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("hello1");
+		  System.out.println("hello2");
+		  System.out.println("hello3");
 	}
 
 }
